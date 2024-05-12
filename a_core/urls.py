60ba4d9b4ace490045dc0b1ba50df57ce3d47e31
vikas swami <swamivikas2004@gmail.com>
2024-05-12 17:15:21 +0530

    Add files via upload

---
+++ a_core/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for a_core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from a_posts.views import *
+from a_users.views import *
+from django.conf import *
+from django.conf.urls.static import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', home_view, name= 'home'),
+    path('post/create/', post_create_view, name = 'post-create'),
+    path('post/delete/<pk>/', post_delete_view, name= 'post-delete'),
+    path('post/edit/<pk>/', post_edit_view, name= 'post-edit'),
+    path('post/<pk>/', post_page_view, name= 'post'),
+    path('profile/', profile_view , name= 'profile'),
+    path('profile/edit/', profile_edit_view , name= 'profile-edit'),
+    path('commentsent/<pk>/', comment_sent , name= 'comment-sent'),
+    
+    
+    
+]
+
+
